85e1595b0673ecf3f7284fed0ec744d84f3b6f30
Jeff Pratt <jpratt@proofpoint.com>
2026-01-07 16:15:52 -0700

    Remove commented plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -64,28 +64,6 @@ require("lazy").setup({
       dependencies = { 'uga-rosa/utf8.nvim' },
       cmd = 'ElectricQuotesToggle',
     },
-    -- {
-      -- "scalameta/nvim-metals",
-      -- ft = { "scala", "sbt", "java" },
-      -- opts = function()
-        -- local metals_config = require("metals").bare_config()
-        -- metals_config.on_attach = function(client, bufnr)
-          -- -- your on_attach function
-        -- end
-
-        -- return metals_config
-      -- end,
-      -- config = function(self, metals_config)
-        -- local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-        -- vim.api.nvim_create_autocmd("FileType", {
-          -- pattern = self.ft,
-          -- callback = function()
-            -- require("metals").initialize_or_attach(metals_config)
-          -- end,
-          -- group = nvim_metals_group,
-        -- })
-      -- end
-    -- },
     { 'nvim-treesitter/nvim-treesitter',
       build = ':TSUpdate',
       config = function()
@@ -166,8 +144,6 @@ require("lazy").setup({
       end,
     },
     { 'clementi/ghostty-dark.nvim', priority = 1000, },
-    -- { 'w0ng/vim-hybrid' },
-    -- { 'ColinKennedy/hybrid2.nvim' },
     { 'neovimhaskell/haskell-vim' },
     { 'pangloss/vim-javascript' },
     { 'stefanos82/nelua.vim' },
